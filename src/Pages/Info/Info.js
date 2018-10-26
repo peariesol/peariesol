a6cfd3fca9a8e8aa7929efdbc296a89117651fb5
Perry Fustero <pjfust@gmail.com>
2018-10-26 15:34:47 -0400

    work on info page

--- src/Pages/Info/Info.js
+++ src/Pages/Info/Info.js
@@ -3,27 +3,47 @@ import links from '../../Utils/Press'
 import quotes from '../../Utils/PressQuotes'
 import './Info.css'
 
+const pressLinks = links.map(({ value, text, link }) => (
+	<span key={value} className="pressLinkBox">
+		<a href={link}>
+			<p className="pressLink">{text}</p>
+		</a>
+	</span>
+))
+
+const pressQuotes = quotes.map(({ value, quote, outlet }) => (
+	<span key={value} className="pressQuoteBox">
+		<p className="pressQuote">
+			<em>{`"${quote}"`}</em>
+			{` - ${outlet}`}
+		</p>
+	</span>
+))
+
 const Info = () => {
-	const pressLinks = links.map(({ value, text, link }) => (
-		<div key={value} className="pressLinkBox">
-			<a href={link}>
-				<p className="pressLink">{text}</p>
-			</a>
-		</div>
-	))
-	const pressQuotes = quotes.map(({ value, quote, outlet }) => (
-		<div key={value} className="pressQuoteBox">
-			<p className="pressQuote">
-				<em>{`"${quote}"`}</em>
-				{` - ${outlet}`}
-			</p>
-		</div>
-	))
 	return (
-		<div>
-			<h2>Info</h2>
-			<div className="PressQuoteContainer">{pressLinks}</div>
-			<div className="PressLinkContainer">{pressQuotes}</div>
+		<div className="infoPage">
+			<div className="contactInfo">
+				<h2>contact info</h2>
+
+				<a
+					style={{ color: 'red' }}
+					href="mailto:solpearie@gmail.com?Subject=Hello%20Dolly"
+					target="_top"
+				>
+					Email Me
+				</a>
+			</div>
+			<div className="pressContainer">
+				<div className="linksContainer">
+					<h2 className="linksHeading">Press</h2>
+					<div className="pressLinks">{pressLinks}</div>
+				</div>
+				<div className="quotesContainer">
+					<h2 className="quotesHeading">Quotes</h2>
+					<div className="pressQuotes">{pressQuotes}</div>
+				</div>
+			</div>
 		</div>
 	)
 }
